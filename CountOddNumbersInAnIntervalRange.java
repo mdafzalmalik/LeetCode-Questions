1a2fe72cb407a2db4e9e1df6cc49bab9dd863c88
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-07-08 00:19:58 +0530

    Update CountOddNumbersInAnIntervalRange.java

--- CountOddNumbersInAnIntervalRange.java
+++ CountOddNumbersInAnIntervalRange.java
@@ -10,13 +10,16 @@ public class CountOddNumbersInAnIntervalRange {
 	}
 	
 	public static int countNoOfOdds(int low, int high) {
-		int count = 0;
-		for(int i = low; i <= high; i++) {
-			if(i % 2 != 0) {
-				count++;
+		int total = high - low + 1;
+		if(total % 2 == 0) {
+			return total/2;
+		}else {
+			if(low % 2 == 0) {
+				return total/2;
+			}else {
+				return (total/2) + 1;
 			}
 		}
-		return count;
 	}
 
 }
